b227cb12418104134f5d70fbef73dfe6b45528f7
Govind <66307025+govindup63@users.noreply.github.com>
2024-09-01 20:06:41 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for the backend API
-const BASE_URL = 'http://13.51.198.4:3001/api/organization';
+const BASE_URL = 'http://172.31.45.171:3001/api/organization';
 
 // Create an instance of axios with default settings
 const api = axios.create({
